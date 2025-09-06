35d2675a7b1e62b1ee052df4563b5cf2d5519da5
riwahoussari <houssaririwa@gmail.com>
2025-09-06 11:18:49 +0300

    Connect postgresql

---
+++ LearningCenter/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearningCenter.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration _config;
+
+        public AppDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(_config.GetConnectionString("WebApiDb"));
+        }
+
+
+        public DbSet<TestTable> TestTables { get; set; }
+    }
+}
